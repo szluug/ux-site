793e8005d72b8f26eaa81454b99db85a0be23c57
mnkzjc <mnk.zajac@gmail.com>
2021-05-21 20:28:00 +0200

    Add header and footer to all sites

--- backend/app.py
+++ backend/app.py
@@ -1,16 +1,26 @@
 from flask import Flask
 from flask import render_template
+from flask import url_for
 
 app = Flask(__name__)
 
 
 @app.route('/')
+@app.route('/index.html')
 def hello_world():
-    return render_template('index.html')
+    return render_template('index.html', show_hero_section=True)
 
-@app.route('/hoo')
-def hello_world2():
-    return render_template('hello.html', name='Patryk i Kuba')
+@app.route('/blog.html')
+def blog():
+    return render_template('blog.html')
+
+@app.route('/blog-single.html')
+def blog_single():
+    return render_template('blog-single.html')
+
+@app.route('/portfolio-details.html')
+def portfolio_details():
+    return render_template('portfolio-details.html')       
 
 if __name__ == '__main__':
     app.run(debug=True)
